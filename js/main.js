c399e97db042f1ebf2bc496259d0b5485cfef161
AmirElbadry <130234122+AmirElbadry@users.noreply.github.com>
2024-02-20 18:44:44 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,91 @@
+(function ($) {
+  "use strict";
+
+  // Dropdown on mouse hover
+  $(document).ready(function () {
+    function toggleNavbarMethod() {
+      if ($(window).width() > 992) {
+        $(".navbar .dropdown")
+          .on("mouseover", function () {
+            $(".dropdown-toggle", this).trigger("click");
+          })
+          .on("mouseout", function () {
+            $(".dropdown-toggle", this).trigger("click").blur();
+          });
+      } else {
+        $(".navbar .dropdown").off("mouseover").off("mouseout");
+      }
+    }
+    toggleNavbarMethod();
+    $(window).resize(toggleNavbarMethod);
+  });
+
+  // Date and time picker
+  $(".date").datetimepicker({
+    format: "L",
+  });
+  $(".time").datetimepicker({
+    format: "LT",
+  });
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
+
+  // Portfolio isotope and filter
+  var portfolioIsotope = $(".portfolio-container").isotope({
+    itemSelector: ".portfolio-item",
+    layoutMode: "fitRows",
+  });
+  $("#portfolio-flters li").on("click", function () {
+    $("#portfolio-flters li").removeClass("active");
+    $(this).addClass("active");
+
+    portfolioIsotope.isotope({ filter: $(this).data("filter") });
+  });
+
+  // Testimonials carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 5000,
+    items: 1,
+    dots: false,
+    loop: true,
+  });
+})(jQuery);
+var index = 0;
+var slides = document.querySelectorAll(".slides");
+var dot = document.querySelectorAll(".dot");
+
+function changeSlide() {
+  if (index < 0) {
+    index = slides.length - 1;
+  }
+
+  if (index > slides.length - 1) {
+    index = 0;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    dot[i].classList.remove("active");
+  }
+
+  slides[index].style.display = "block";
+  dot[index].classList.add("active");
+
+  index++;
+
+  setTimeout(changeSlide, 7000);
+}
+
+changeSlide();
